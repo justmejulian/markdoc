3f0795f71601f67e27b52b30cec0932a540f84a6
emberrap <emberrap@students.zhaw.ch>
2018-03-05 23:43:23 +0000

    Reworked parser completely. Still WIP though

--- src/App.js
+++ src/App.js
@@ -4,32 +4,32 @@ import './App.css';
 
 import Editor from './models/Editor';
 import Preview from './models/Preview';
-import parser from './parser.js';
+import HtmlDOM from './parser.js';
 
 // Old
 //var marked = require('marked');
 
 class App extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        value: ""
-      }
-      this.createMarkup = this.createMarkup.bind(this);
+  constructor() {
+    super();
+    this.state = {
+      value: ""
     }
+    this.createMarkup = this.createMarkup.bind(this);
+  }
 
-    handleChange (value) {
-      this.setState({
-        value: parser(value)
-      });
-    }
+  handleChange (value) {
+    this.setState({
+      value: HtmlDOM.fromSource(value).toHtml()
+    });
+  }
 
-    // ToDo: Move this to the Preview or some other class
-    createMarkup() {
-      return {
-        __html: this.state.value
-      };
-    }
+  // ToDo: Move this to the Preview or some other class
+  createMarkup() {
+    return {
+      __html: this.state.value
+    };
+  }
 
   render() {
     return (
--- src/parser.js
+++ src/parser.js
@@ -1,20 +1,170 @@
-function parser(src) {
-    var parsed = src;
-    // Headers
-    parsed = parsed.replace(/^# (.*?)$/gm, "<h1>$1</h1>");
-    parsed = parsed.replace(/^## (.*?)$/gm, "<h2>$1</h2>");
-    parsed = parsed.replace(/^### (.*?)$/gm, "<h3>$1</h3>");
-    parsed = parsed.replace(/^#### (.*?)$/gm, "<h4>$1</h4>");
-    parsed = parsed.replace(/^##### (.*?)$/gm, "<h5>$1</h5>");
-    parsed = parsed.replace(/^###### (.*?)$/gm, "<h6>$1</h6>");
 
-    // Emphasis
-    parsed = parsed.replace(/\*\*(.+?)\*\*/g, "<strong>$1</strong>");
-    parsed = parsed.replace(/__(.+?)__/g, "<strong>$1</strong>");
-    parsed = parsed.replace(/\*(.+?)\*/g, "<em>$1</em>");
-    parsed = parsed.replace(/_(.+?)_/g, "<em>$1</em>");
-    parsed = parsed.replace(/~~(.+?)~~/g, "<s>$1</s>");
-    return parsed;
+// New parser approach using the component pattern
+// TODO: complete existing classes and add missing ones
+// TODO: Solve problem of multiline encompassing elements(lists, quotes, code)
+class HtmlComponent { // done
+    constructor() {
+        this.parent = null;
+        this.children = [];
+    }
+    
+    addChild(component) {
+        component.parent = this;
+        this.children.push(component);
+    }
+    removeChild(component) {
+        if (children.includes(component)) {
+            this.children.splice(children.indexOf(component));
+            component.parent = null;
+        }
+    }
 }
 
-export default parser
+class HtmlDOM extends HtmlComponent {
+    static fromSource(source) {
+        var dom = new HtmlDOM();
+        for (const line of source.split(/\r?\n/g)) {
+            if (HtmlHeader.test(line)) {
+                dom.addChild(HtmlHeader.parse(line));
+                continue;
+            }
+            if (HtmlTOC.test(line)) {
+                dom.addChild(HtmlTOC.parse(line));
+            }
+            //TODO: insert lists, quotes, codeblocks etc.
+            if (HtmlTextLine.test(line)) {
+                dom.addChild(HtmlTextLine.parse(line));
+                continue;
+            }
+        }
+        return dom;
+    }
+    toHtml() { // done
+        var lines = [];
+        for (var component of this.children) {
+            lines.push(component.toHtml());
+        }
+        return lines.join('\n');
+    }
+}
+
+// text components:
+class HtmlText extends HtmlComponent { // done
+    constructor(value) {
+        super();
+        this.value = value;
+    }
+
+    static test(line) {
+        return true;
+    }
+
+    static parse(line) { // NOTE: Does not account for unbalanced parenthesis
+        line = line.replace(/\*\*(.+?)\*\*/g, "<strong>$1</strong>");
+        line = line.replace(/__(.+?)__/g, "<strong>$1</strong>");
+        line = line.replace(/\*(.+?)\*/g, "<em>$1</em>");
+        line = line.replace(/_(.+?)_/g, "<em>$1</em>");
+        line = line.replace(/~~(.+?)~~/g, "<s>$1</s>");
+        return new HtmlText(line);
+    }
+
+    toHtml() {
+        return this.value;
+    }
+}
+
+// per line components:
+class HtmlTextLine extends HtmlComponent {
+    constructor(value) {
+        super();
+        this.addChild(HtmlText.parse(value));
+    }
+
+    static test(line) {
+        return true;
+    }
+
+    static parse(line) {
+        return new HtmlTextLine(line);
+    }
+
+    toHtml() {
+        var tags = ["<p>"];
+        for (var component of this.children) {
+            tags.push(component.toHtml());
+        }
+        tags.push("</p>");
+        return tags.join("");
+    }
+}
+
+class HtmlHeader extends HtmlComponent {
+    constructor(value, level) {
+        super();
+        this.level = level;
+        this.addChild(HtmlText.parse(value));
+    }
+
+    static test(line) {
+        return /^#{1,6} .+?$/gm.test(line);
+    }
+
+    static parse(line) {
+        var title = line.replace(/^#{1,6} (.+?)$/gm, "$1");
+        var level = line.replace(/^(#{1,6}) .+?$/gm, "$1").length;
+        return new HtmlHeader(title, level);
+    }
+
+    toHtml() {
+        var pretag = "<h" + this.level + ">";
+        var posttag = "</h" + this.level + ">";
+        var tags = [pretag];
+        for (var component of this.children) {
+            tags.push(component.toHtml());
+        }
+        tags.push(posttag);
+        return tags.join("");
+    }
+}
+
+class HtmlTOC extends HtmlComponent {
+
+    static test(line) {
+        return /^@TOC$/gm.test(line);
+    }
+
+    static parse(line) {
+        return new HtmlTOC();
+    }
+
+    toHtml() {
+        var tags = ["<div id=\"toc\" class=\"toc\">"];
+        for (var component of this.children) {
+            tags.push(component.toHtml());
+        }
+        tags.push("</div>");
+        return tags.join("");
+    }
+
+    compile() {
+        this.children = [];
+        for (var component of this.parent.children) {
+            if (component instanceof HtmlHeader) {
+                //TODO: Decide on how to make the TOC
+            }
+        }
+    }
+}
+
+// For testing:
+// console.log(HtmlDOM.fromSource(
+//     "## t_~~e~~**st**_\n" +
+//     "Hi **there**!"
+// ).toHtml());
+
+
+export default {
+    HtmlComponent, HtmlDOM,
+    HtmlText,
+    HtmlHeader, HtmlTextLine, HtmlTOC
+}
