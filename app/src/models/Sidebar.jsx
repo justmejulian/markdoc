79beee46641233ae8e60e3f07794b487cb90d5d0
Lukas Thöni <thoenluk@students.zhaw.ch>
2018-03-15 20:41:26 +0100

    Added expand and collapse button using a clever idea someone else had.

--- app/src/models/Sidebar.jsx
+++ app/src/models/Sidebar.jsx
@@ -8,13 +8,19 @@ export default class Sidebar extends Component {
 		super();
 		this.handleDateChange = (date) => this._handleDateChange(date);
 		this.handleFieldChange = () => this._handleFieldChange();
+		this.handleExpandOrCollapse = () => this._handleExpandOrCollapse();
 		this.state = {
 			date: new Date(),
-		}
+			isCollapsed: true,
+		};
 	}
 	
 	_handleDateChange(date) {
 		this.setState({ date });
+	}
+	
+	_handleExpandOrCollapse(){
+		this.setState({isCollapsed: !this.state.isCollapsed});
 	}
 	
 	_handleFieldChange() {
@@ -22,14 +28,25 @@ export default class Sidebar extends Component {
 	}
 
   render() {
-    return (
+    let content;
+	
+	if(!this.state.isCollapsed){
+		content = (
+			<div>
+				<p>Title:</p><input type='text' onChange={this.handleFieldChange} />
+				<p>Author:</p><input type='text' onChange={this.handleFieldChange} />
+				<p>Date:</p><DatePicker onChange={this.handleDateChange} value={this.state.date} />
+				<p>Header:</p><input type='text' onChange={this.handleFieldChange} /><input type='text' onChange={this.handleFieldChange} /><input type='text' onChange={this.handleFieldChange} />
+				<p>Footer:</p><input type='text' onChange={this.handleFieldChange} /><input type='text' onChange={this.handleFieldChange} /><input type='text' onChange	={this.handleFieldChange} />
+			</div>
+			);
+	}
+	let expandOrCollapse = this.state.isCollapsed ? '>>' : '<<';
+	return (
       <div>
-	  <p>Title:</p><input type='text' onChange={this.handleFieldChange} />
-	  <p>Author:</p><input type='text' onChange={this.handleFieldChange} />
-	  <p>Date:</p><DatePicker onChange={this.handleDateChange} value={this.state.date} />
-	  <p>Header:</p><input type='text' onChange={this.handleFieldChange} /><input type='text' onChange={this.handleFieldChange} /><input type='text' onChange={this.handleFieldChange} />
-	  <p>Footer:</p><input type='text' onChange={this.handleFieldChange} /><input type='text' onChange={this.handleFieldChange} /><input type='text' onChange={this.handleFieldChange} />
-      </div>
+		<button onClick={this.handleExpandOrCollapse}>{expandOrCollapse}</button>
+		{content}
+	  </div>
     )
   }
 }
