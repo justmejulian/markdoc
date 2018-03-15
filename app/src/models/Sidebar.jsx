3e2e429086dc11425ef7361fbbf75eb7dedea2ae
Lukas Thöni <thoenluk@students.zhaw.ch>
2018-03-15 22:17:22 +0100

    Removed test function from Sidebar.jsx, fixed its display, and added TODOS. Sidebar is finished until further development in the elements that use the values it provides.

--- app/src/models/Sidebar.jsx
+++ app/src/models/Sidebar.jsx
@@ -3,6 +3,7 @@ import DatePicker from 'react-date-picker';
 
 export default class Sidebar extends Component {
 	//🐘
+	//TODO: Send field values to appropriate places, meaning header and footer and I imagine the other fields have to be queried somewhere.
 	
 	constructor() {
 		super();
@@ -24,7 +25,8 @@ export default class Sidebar extends Component {
 	}
 	
 	_handleFieldChange() {
-		console.log('penis');
+		//TODO: Probably split this function into multiple to distinguish which field was changed.
+		//Also TODO: Actually send the changed value someplace.
 	}
 
   render() {
@@ -33,9 +35,10 @@ export default class Sidebar extends Component {
 	if(!this.state.isCollapsed){
 		content = (
 			<div>
+				<h1>Sidebar of sidebariness!</h1> {/* Note: This provides necessary space for the datepicker to display. Do not remove without replacement! */}
 				<p>Title:</p><input type='text' onChange={this.handleFieldChange} />
-				<p>Author:</p><input type='text' onChange={this.handleFieldChange} />
-				<p>Date:</p><DatePicker onChange={this.handleDateChange} value={this.state.date} />
+				<p><br></br>Author:</p><input type='text' onChange={this.handleFieldChange} />
+				<p><br></br>Date:</p><DatePicker onChange={this.handleDateChange} value={this.state.date} />
 				<p>Header:</p><input type='text' onChange={this.handleFieldChange} /><input type='text' onChange={this.handleFieldChange} /><input type='text' onChange={this.handleFieldChange} />
 				<p>Footer:</p><input type='text' onChange={this.handleFieldChange} /><input type='text' onChange={this.handleFieldChange} /><input type='text' onChange	={this.handleFieldChange} />
 			</div>
