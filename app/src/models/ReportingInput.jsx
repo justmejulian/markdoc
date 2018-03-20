008c7405955eadc12d31107fe6990fd066851cbf
Lukas Thöni <thoenluk@students.zhaw.ch>
2018-03-20 15:33:13 +0100

    Made sidebar expand button only show up while the mouse is over the left edge of the screen. Visuals subject to change. Sidebar now automatically closes when hidden.
    Bugfix: Input fields now persist their values across sidebar being opened and closed. I can imagine that might be important.

--- app/src/models/ReportingInput.jsx
+++ app/src/models/ReportingInput.jsx
@@ -4,15 +4,22 @@ export default class Sidebar extends Component {
 	
 	constructor(props){
 		super(props);
-		this.handleChange = props.handleChange;
+		this.handleFieldChange = props.handleFieldChange;
+		this.handleChange = (index, evt) => this._handleChange(index, evt);
 		this.state = {
 			index: props.index,
+			value: props.value,
 		}
+	}
+	
+	_handleChange(index, evt){
+		this.setState({value: evt.target.value})
+		this.handleFieldChange(this.state.index, evt);
 	}
 	
 	render(){
 		return (
-			<input type='text' onChange={evt => this.handleChange(this.state.index, evt)} />
+		<input type='text' onChange={evt => this.handleChange(this.state.index, evt)} value={this.state.value}/>
 		)
 	}
 }
--- app/src/models/Sidebar.jsx
+++ app/src/models/Sidebar.jsx
@@ -6,15 +6,17 @@ export default class Sidebar extends Component {
 	//🐘
 	//TODO: Send field values to appropriate places, meaning header and footer and I imagine the other fields have to be queried somewhere.
 	
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
 		this.handleDateChange = (date) => this._handleDateChange(date);
 		this.handleFieldChange = (index, evt) => this._handleFieldChange(index, evt);
 		this.handleExpandOrCollapse = () => this._handleExpandOrCollapse();
+		this.handleMouseHover = () => this._handleMouseHover();
 		this.state = {
 			date: new Date(),
 			isCollapsed: true,
-			values: ["", "", "", "", "", "", "",]
+			values: ["", "", "", "", "", "", ""],
+			isHovering: false,
 		};
 	}
 	
@@ -34,29 +36,46 @@ export default class Sidebar extends Component {
 		newValues[index] = value;
 		this.setState({values: newValues});
 	}
+	
+	_handleMouseHover(){
+		this.setState({isHovering: !this.state.isHovering});
+		this.setState({isCollapsed: true});
+	}
 
   render() {
-    let content;
+    var content;
 	var boundHandleFieldChange = this.handleFieldChange.bind(this);
-	
 	if(!this.state.isCollapsed){
 		content = (
 			<div>
 				<h1>Sidebar of sidebariness!</h1> {/* Note: This provides necessary space for the datepicker to display. Do not remove without replacement! */}
-				<p>Title:</p><ReportingInput handleChange = {boundHandleFieldChange} index = {0} />
-				<p><br></br>Author:</p><ReportingInput handleChange = {boundHandleFieldChange} index = {1} />
+				<p>Title:</p>
+				<ReportingInput handleFieldChange = {boundHandleFieldChange} index = {0} value={this.state.values[0]}/>
+				<p><br></br>Author:</p>
+				<ReportingInput handleFieldChange = {boundHandleFieldChange} index = {1} value={this.state.values[1]}/>
 				<p><br></br>Date:</p><DatePicker onChange={this.handleDateChange} value={this.state.date} />
-				<p>Header:</p><ReportingInput handleChange = {boundHandleFieldChange} index = {2} /><ReportingInput handleChange = {boundHandleFieldChange} index = {3} /><ReportingInput handleChange = {boundHandleFieldChange} index = {4} />
-				<p>Footer:</p><ReportingInput handleChange = {boundHandleFieldChange} index = {5} /><ReportingInput handleChange = {boundHandleFieldChange} index = {6} /><ReportingInput handleChange = {boundHandleFieldChange} index = {7} />
+				<p>Header:</p>
+				<ReportingInput handleFieldChange = {boundHandleFieldChange} index = {2} value={this.state.values[2]}/>
+				<ReportingInput handleFieldChange = {boundHandleFieldChange} index = {3} value={this.state.values[3]}/>
+				<ReportingInput handleFieldChange = {boundHandleFieldChange} index = {4} value={this.state.values[4]}/>
+				<p>Footer:</p>
+				<ReportingInput handleFieldChange = {boundHandleFieldChange} index = {5} value={this.state.values[5]}/>
+				<ReportingInput handleFieldChange = {boundHandleFieldChange} index = {6} value={this.state.values[6]}/>
+				<ReportingInput handleFieldChange = {boundHandleFieldChange} index = {7} value={this.state.values[7]}/>
 			</div>
 			);
 	}
-	let expandOrCollapse = this.state.isCollapsed ? '>>' : '<<';
+	var expandOrCollapse = this.state.isCollapsed ? '>>' : '<<';
+	var style = this.state.isCollapsed ? {width: '5vw'} : {width: '45vw'};
 	return (
-      <div id='sidebar'>
-		<button onClick={this.handleExpandOrCollapse} id='sidebar-expand-button'>{expandOrCollapse}</button>
-		{content}
-	  </div>
-    )
+			<div id='sidebar' style={style} onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}>
+					{this.state.isHovering &&
+						<div>
+						<button onClick={this.handleExpandOrCollapse} id='sidebar-expand-button'>{expandOrCollapse}</button>
+						{content}
+						</div>
+					}
+			</div>
+		)
   }
 }
