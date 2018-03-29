76e6a2d15c5e32f73bfdb84f8e8081e031b6b4cb
meierph4 <meierph4@students.zhaw.ch>
2018-03-29 11:16:18 +0200

    Added Tests to Word Counter

--- test/WordCounter-Test.js
+++ test/WordCounter-Test.js
@@ -5,4 +5,24 @@ describe('WordCounter', () => {
   it('Should count words in the document', () => {
     expect(WordCounter.countWords('hello this is a test')).toEqual(5);
   });
+
+  it('Should count double distance as word', () => {
+    expect(WordCounter.countWords('  ')).toEqual(1);
+  });
+
+  it('Should count a lot of words', () => {
+    expect(
+      WordCounter.countWords(
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+      )
+    ).toEqual(100);
+  });
+
+  it('Should count numbers', () => {
+    expect(WordCounter.countWords('5')).toEqual(1);
+  });
+
+  it('Should count as one word', () => {
+    expect(WordCounter.countWords('')).toEqual(1);
+  });
 });
