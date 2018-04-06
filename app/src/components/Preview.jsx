0781e513e567ae15beaa485d2cc3784e7765b72f
Lukas Thöni <thoenluk@students.zhaw.ch>
2018-04-06 12:04:27 +0200

    Moved zoom buttons and added icons to them.

--- app/src/components/Preview.jsx
+++ app/src/components/Preview.jsx
@@ -7,6 +7,7 @@ class Preview extends React.Component {
     super(props);
     this.setPreview = this.setPreview.bind(this);
     this.handleZoomIn = this._handleZoomIn.bind(this);
+    this.handleZoomOut = this._handleZoomOut.bind(this);
     this.state = {
       pages: [{ key: 0, html: Store.getMarkdown(), height: 0 }],
       words: [],
@@ -47,6 +48,16 @@ class Preview extends React.Component {
       this.setState(newState);
     } else {
       alert('Maximum zoom has been reached!');
+    }
+  }
+
+  _handleZoomOut() {
+    if (this.state.zoom > 0.5) {
+      var newState = this.state;
+      newState.zoom -= 0.1;
+      this.setState(newState);
+    } else {
+      alert('Minimum zoom has been reached!');
     }
   }
 
@@ -97,18 +108,39 @@ class Preview extends React.Component {
 
   render() {
     return (
-      <div id="preview" style={{ zoom: this.state.zoom }}>
-        <button className="zoomButton" onClick={this.handleZoomIn}>
-          Zoom in
-        </button>
-        {this.state.pages.map(page => (
-          <Page
-            id={page.key}
-            key={page.key}
-            html={page.html}
-            handleHeight={this.handleHeight.bind(this)}
-          />
-        ))}
+      <div id="preview">
+        <div id="zoomButtonContainer">
+          <button className="zoomButton" onClick={this.handleZoomIn}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zm2.5-4h-2v2H9v-2H7V9h2V7h1v2h2v1z" />
+            </svg>
+          </button>
+          <button className="zoomButton" onClick={this.handleZoomOut}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zM7 9h5v1H7z" />
+            </svg>
+          </button>
+        </div>
+        <div style={{ zoom: this.state.zoom }}>
+          {this.state.pages.map(page => (
+            <Page
+              id={page.key}
+              key={page.key}
+              html={page.html}
+              handleHeight={this.handleHeight.bind(this)}
+            />
+          ))}
+        </div>
       </div>
     );
   }
