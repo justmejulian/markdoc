5a55ef945b0fb65890b4164256bc5c86cac6bd8f
Alexandros Soultanis <soultale@students.zhaw.ch>
2018-04-21 22:36:23 +0200

    merged wordcounter-test

--- __tests__/wordcounter.test.js
+++ __tests__/wordcounter.test.js
@@ -1,5 +1,6 @@
 'use strict';
-const { WordCounter } = require('../app/src/js/wordcount');
+const WordCounter = require('../app/src/js/wordcount');
+const CharCounter = require('../app/src/js/wordcount');
 
 describe('WordCounter', () => {
   it('should count words in the document', () => {
@@ -22,5 +23,22 @@ describe('WordCounter', () => {
     expect(WordCounter.countWords('... with punctuation there ...')).toEqual(3);
     expect(WordCounter.countWords('But what about this\n?')).toEqual(4);
     expect(WordCounter.countWords('YOU `SHALL ` PASS!!')).toEqual(3);
+  });
+});
+
+describe('CharCounter', () => {
+  it('should count every single character in the document', () => {
+    expect(CharCounter.countCharacters('Hey, this is a test.')).toEqual(20);
+    expect(CharCounter.countCharacters('Hello')).toEqual(5);
+  });
+  it('should not count spaces', () => {
+    expect(CharCounter.countCharacters(' ')).toEqual(0);
+    expect(CharCounter.countCharacters('    ')).toEqual(0);
+  });
+  it('should count special character', () => {
+    expect(CharCounter.countCharacters('ü¨ö$ä+@^')).toEqual(8);
+  });
+  it('should show zero, if no it has no text', () => {
+    expect(CharCounter.countCharacters('')).toEqual(0);
   });
 });
