9a6d469106bb9dd87f29f4dd758b12086323791c
Alexandros Soultanis <soultale@students.zhaw.ch>
2018-04-22 14:59:35 +0200

    bug fixed in charcounter

--- app/src/js/wordcount.js
+++ app/src/js/wordcount.js
@@ -1,15 +1,23 @@
-function countWords(str) {
-  var strs = str.trim().split(' ');
-  var count = strs.length;
-  return count;
-}
+class WordCounter {
+  static countWords(str) {
+    var strs = str.trim().split(/[\s\n\r\.\?\!\(\)\[\]\{\}\`]+/);
+    var count = 0;
+    for (const string of strs) {
+      count += string.length == 0 ? 0 : 1;
+    }
+    return count;
+  }
 
-function countCharacters(str) {
-  var count = str.length;
-  return count;
+  static countCharacters(str) {
+    var strs = str.trim();
+    var count = 0;
+    for (const string of strs) {
+      count += string.length == 0 ? 0 : 1;
+    }
+    return count;
+  }
 }
 
 module.exports = {
-  countWords: countWords,
-  countCharacters: countCharacters
+  WordCounter: WordCounter
 };
