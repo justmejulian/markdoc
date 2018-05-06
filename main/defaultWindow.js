dfed04b120e0c075faf0ba105620e4e374661ade
Raphael Gurtner <gurtnrap@students.zhaw.ch>
2018-05-06 23:50:25 +0200

    Fixed incorrect icon path...
    ... whopsy. Now works on linux

--- main/defaultWindow.js
+++ main/defaultWindow.js
@@ -4,5 +4,5 @@ module.exports = {
   width: 1400,
   height: 1000,
   titleBarStyle: 'hidden',
-  icon: path.join(__dirname, '../app/src/icons/png/64x64.png')
+  icon: path.join(__dirname, '../app/src/icons/png/markdoc_256x256.png')
 };
